7e96d465b6f05194b08dd5b2eaf15a0ad979e3b8
Олег Белый <belochard@gmail.com>
2022-09-18 05:21:41 +0300

    Added exception handling

--- IntelliasTT/Source.cpp
+++ IntelliasTT/Source.cpp
@@ -12,46 +12,48 @@ using std::cout;
 using std::cin;
 
 
-
-
-
-
 int main() {
-	char separator = '*';
 	string fin_name, fout_name;
+	char separator;
+
 	cout << "Plese enter input data file name: ";
 	cin >> fin_name;
-	cout << "Please enter output data file name: ";
-	cin >> fout_name;
 	ifstream fin(fin_name);
-	ifstream fin("Stuff.txt");
 	if (fin.fail()) {
 		cout << "Incorrect input file name.\n";
 		exit(1);
 	}
-	//ofstream fout(fout_name);
+
+	cout << "Please enter output data file name: ";
+	cin >> fout_name;
 	ofstream fout("ModifiedStuff.txt");
 	if (fout.fail()) {
 		cout << "Incorrect output file name.\n";
 		exit(1);
 	}
+
+	cout << "Please enter separator in the file: ";
+	cin >> separator;
+	
+	
 	string line, name, date, hours_worked;
 
-	while (!fin.eof()) {
+	try {
+		while (!fin.eof()) {
 
-		getline(fin, line);
+			getline(fin, line);
 
-		name = getName(line, separator);
-		date = getDate(line, separator);
-		modifyDate(date);
-		hours_worked = getHoursWorked(line, separator);
-		fout << "Name: " << name << ", date: " << date << ", hours worked: " << hours_worked << ".\n";
+			name = getName(line, separator);
+			date = getDate(line, separator);
+			modifyDate(date);
+			hours_worked = getHoursWorked(line, separator);
+			fout << "Name: " << name << ", date: " << date << ", hours worked: " << hours_worked << ".\n";
+		}
 	}
-	
-	
-	
+	catch (...) {
+		cout << "Most likely you've entered wrong separator. Check it.\n";
+	}
 	
 	fin.close();
 	fout.close();
 }
-
